8124b65903d07379f66f62185b52d93e7f8676c7
DmitriyVladimirovich <150802655+DmitriyVladimirovich@users.noreply.github.com>
2023-11-21 16:03:32 +0300

    Update S11T2.py
    Урок 11, задание 2

--- S11T2.py
+++ S11T2.py
@@ -1,10 +1,15 @@
 #--
-# Структура 0-pets, 1-key, 2-pname, 3-ТТХ
+#Структура 0-pets, 1-key, 2-pname, 3-ТТХ
 #--
+
 def create(pets):
 #    import collections                             #не работает
 #    last = collections.deque(pets, maxlen=1)[0]    #не работает
-    last=len(pets)
+    if len(pets)==0:
+        last=0
+    else:
+        last=[i for i in pets.keys()][-1]+1
+#        print('Testcount-',last)
     pet=dict()
     ttx=dict()
     typ=input('Укажите вид питомца: ')
@@ -27,6 +32,27 @@ def read(pets, key):
 def get_pet(pets,i):
   return pets[i] if i in pets.keys() else False
 
+def update(pets,key):
+    pet=dict()
+    ttx=dict()
+    typ=input('Измените вид питомца: ')                     #для коммерции можно добавить обработку "enter - не менять значение".
+    pname=input('Измените имя питомца: ')
+    age=int(input('Измените возраст питомца: '))
+    mastername=input('Измените имя хозяина: ')
+    ttx['Вид питомца']=typ
+    ttx['Возраст питомца']=age
+    ttx['Имя хозяина']=mastername
+    pet[pname]=ttx
+    pets[key]=pet
+    return pets
+
+def delete(pets,key):
+    if key in pets:
+        del pets[key]
+        return pets
+    else:
+        return False
+
 def get_suffix(x):
   teens={11,12,13,14,15,16,17,18,19}
   tails={0,5,6,7,8,9}
@@ -43,12 +69,18 @@ def pets_list(pts):
     read(pts,i)
 
 pets=dict()
-command=input('Введите команду (create, list, stop): ')
+command=input('Введите команду (create, list, update, delete, stop): ')
 while command!='stop':
     if command=='create':
         create(pets)
     elif command=='list':
         pets_list(pets)
+    elif command=='update':
+        a=int(input('Введите цифровой индекс изменяемой записи:'))
+        update(pets,a)
+    elif command=='delete':
+        a=int(input('Введите цифровой индекс изменяемой записи:'))
+        delete(pets,a)
     else:
         print('Неверная команда, повторите ввод: ')
-    command=input('Введите команду (create, list, stop): ')
+    command=input('Введите команду (create, list, update, delete, stop): ')
